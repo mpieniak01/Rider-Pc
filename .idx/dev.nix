d3c696c4cc639b0063854b27a1e0bd78cf018fe0
pppnews <pppnews@gmail.com>
2025-12-05 13:34:06 +0000

    Refactor: Improve formatting and fix syntax in dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,13 +1,18 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
-{ pkgs, ... }: {
+{
+  # To learn more about how to use Nix to configure your environment
+  # see: https://firebase.google.com/docs/studio/customize-workspace
+  pkgs,
+  ...
+}: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages.
-  # All python packages will be auto-exposed to the shell.
   packages = with pkgs; [
+    # General development tools
     gnumake # For `make` commands
+    
+    # Python environment
     python311 # Base Python interpreter
 
     # Core application dependencies (from requirements.txt)
@@ -19,18 +24,20 @@
     python311Packages.celery
     python311Packages.tomli
     python311Packages.tomli-w
-    (python311Packages.python-dotenv.overrideAttrs (old: {
-      # Fix build issue in Nix: https://github.com/NixOS/nixpkgs/issues/231583
-      postInstall = '''
-        mv $out/bin/dotenv $out/bin/python-dotenv
-      ''';
-    }))
+    (
+      python311Packages.python-dotenv.overrideAttrs (old: {
+        # Fix build issue in Nix: https://github.com/NixOS/nixpkgs/issues/231583
+        postInstall = ''
+          mv $out/bin/dotenv $out/bin/python-dotenv
+        '';
+      })
+    )
     python311Packages.google-auth
     python311Packages.google-auth-oauthlib
     python311Packages.grpcio
     python311Packages.protobuf
 
-    # Testing dependencies
+    # Testing and linting dependencies
     ruff
     python311Packages.pytest
     python311Packages.pytest-asyncio
@@ -43,8 +50,9 @@
     PYTHONPATH = "${pkgs.python311.sitePackages}";
   };
 
+  # IDX workspace configuration
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    # Search for extensions on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
@@ -54,7 +62,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["make" "start-dev"];
+          command = [ "make" "start-dev" ];
           manager = "web";
         };
       };
